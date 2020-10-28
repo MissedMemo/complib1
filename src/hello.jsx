7461ff46d86471dd2673da0be5877f0cea582c24
Andrew Lloyd <alloyd@divisionsinc.com>
2020-10-27 21:58:08 -0700

    rolled back new jsx syntax

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Hello = () => <div className='hello'>
   Hello, React!
 </div>
--- src/toggle.jsx
+++ src/toggle.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
  
 const ToggleButton = () => {
   const [isToggled, setIsToggled] = useState(false)
